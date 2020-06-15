4c296a9facfbd305bb0369c774693a771e2b7d69
floriansr <sueurfloriansf@gmail.com>
2020-06-15 09:35:47 +0200

    Provider store passed from app.jsx to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,19 @@
 import "antd/dist/antd.css";
-import "assets/scss/styles.scss";
+import "./assets/scss/styles.scss";
 
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "App";
+import { Provider } from "react-redux";
+import store from "redux/store";
+
+import App from "./App";
 
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-		<App />,
+	<Provider store={store}>
+		<App />
+	</Provider>,
 	document.getElementById("root")
 );
 
