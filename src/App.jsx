bd6f5bd5b1fa93632e43decda9f7852cae442d1c
floriansr <sueurfloriansf@gmail.com>
2020-06-15 09:37:32 +0200

    provider store cleaning, refresh page with json.parse token

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "redux/store";
+import { useDispatch } from "react-redux";
 
 import Navbar from "components/Navbar";
 import Authroute from "components/AuthRoute"
@@ -14,16 +13,46 @@ import About from "pages/About";
 import Profile from "pages/Profile";
 import NotFound from "pages/NotFound";
 
+import Cookies from 'js-cookie'
+import { setConnexion, setProfile } from "./redux";
+
 
 
 const App = () => {
+
+	const dispatch = useDispatch()
+
+		useEffect(() => { 
+
+				if (Cookies.get('token')) {
+
+				const token = JSON.parse(Cookies.get('token')).jwt
+				const userStatus = JSON.parse(Cookies.get('token')).status
+
+			    fetch(`https://form-you-back.herokuapp.com/${userStatus}s/sign_in.json`, {
+			      method: 'post',
+			      headers: {
+			        'Authorization': token, 
+			        'Content-Type': 'application/json'
+			      },
+			    })
+			      .then(response => response.json())
+			      .then(response => {
+
+			        if (!response.error) {
+				        dispatch(setProfile(response))
+				        dispatch(setConnexion())
+			        }
+			      })
+			      .catch(error => console.log(error));
+				}
+	}, [dispatch]);
+
 
 	return (
 		<>
 				<Router>
 					<div>
-						<Provider store={store}>
-
 							<Navbar />
 
 							<Switch>
@@ -34,7 +63,6 @@ const App = () => {
 								<Route exact path="/" component={Home} />
 								<Route path="*" component={NotFound} status={404} />
 							</Switch>
-			            </Provider>
 					</div>
 				</Router>
 		</>
