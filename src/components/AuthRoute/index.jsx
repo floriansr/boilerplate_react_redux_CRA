ba15c702cfe98e6eba4d5db6d18e945a0799f20c
floriansr <sueurfloriansf@gmail.com>
2020-06-15 09:15:27 +0200

    this is a private page message

--- src/components/AuthRoute/index.jsx
+++ src/components/AuthRoute/index.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Route } from "react-router-dom";
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import Cookies from 'js-cookie'
+
+import { setConnexion, removeConnexion } from "../../redux";
+
 
 const AuthRoute = ({ component: Component, ...rest } : AuthRoute) => {
 
 	const logStatus = useSelector(state => state.log.log);
+	const dispatch = useDispatch();
+
+	const checkLog = () => (
+		Cookies.get('token') ? dispatch(setConnexion()) : dispatch(removeConnexion())
+	);
 
 	return (
 		<>
-			<Route {...rest} render={props => (logStatus) ? ( <Component {...props} />) : ( <p>Welcome on My Social Network. This website is a training to Redux and React. We use auth and routing to create a small social media website.</p>)} />
+			<Route {...rest} render={props => (checkLog() && logStatus) ? ( <Component {...props} />) : ( <p>This is a private page, go log in please.</p>)} />
 		</>
 	);
 }
